33c5bfb219ead1fc95d6eca32f4e9227bda346fb
Sami12901 <mdsamiislam2006@gmail.com>
2026-06-11 23:47:22 +0600

    feat: build inventory-analyzer tool

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ALL IN ONE Service Worker
-const CACHE_NAME = 'all-in-one-cache-v24';
+const CACHE_NAME = 'all-in-one-cache-v25';
 
 // Static assets to pre-cache immediately on install
 const PRECACHE_ASSETS = [
